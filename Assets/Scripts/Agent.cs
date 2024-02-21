b646362da7ffa8d4de18d5e2940224d8f42888d0
danzabarr <danzabarr@hotmail.co.uk>
2024-02-21 13:50:51 +0000

    sync

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -3,16 +3,50 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+public interface IAgent<Node>
+{
+	/// <summary>
+	/// Set the path for the agent to follow.
+	/// </summary>
+	/// <param name="path"></param>
+	void SetPath(IEnumerable<Node> path);
+
+	/// <summary>
+	/// Removes the current path.
+	/// </summary>
+	void ClearPath();
+
+	/// <summary>
+	/// Implementer returns true if the agent has a path.
+	/// </summary>
+	/// <returns></returns>
+	bool HasPath();
+
+	/// <summary>
+	/// The node at the start of the path.
+	/// </summary>
+	/// <returns></returns>
+	Node GetStart();
+
+	/// <summary>
+	/// The goal node.
+	/// </summary>
+	/// <returns></returns>
+	Node GetGoal();
+
+	/// <summary>
+	/// Implementer must return the node that the agent is currently moving towards.
+	/// </summary>
+	/// <returns></returns>
+	Node GetNext();
+}	
+
+
 public class Agent : MonoBehaviour
 {
 	private List<Node> path = new List<Node>();
-	private Node current;
 
 	public float speed = 1;
-	public void PathTo(Node goal, IGraph<Node> graph)
-	{
-		path = Search.AStar(current, goal, graph, (_, _) => false);
-	}
 
 	public void SetPath(List<Node> path)
 	{
@@ -27,25 +61,21 @@ public class Agent : MonoBehaviour
 			transform.position += direction * speed * Time.deltaTime;
 
 			if (Vector3.Distance(transform.position, NextNode.position) < 0.1f)
-				current = Pop();
+				Pop();
 		}
 	}
 
-	public Node CurrentNode => current;
-
 	public bool HasPath => path != null && path.Count > 0;
 
-	public Node NextNode => HasPath ? path[0] : current;
+	public Node NextNode => HasPath ? path[0] : null;
 
 	public Node Pop()
 	{
-		if (HasPath)
-		{
-			Node next = NextNode;
-			path.RemoveAt(0);
-			return next;
-		}
-		return current;
+		if (!HasPath)
+			return null;
+		Node next = path[0];
+		path.RemoveAt(0);
+		return next;
 	}
 	 
 	public void ClearPath()
